beaea63dedca055f9d2513d99e49e2ef55f1fcae
dftbaustin <austinddd63z@gmail.com>
2018-04-18 12:48:30 -0400

    Node server prints mime type of a handful of file extensions to console

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 var http = require("http");
 
+const FILETYPES = {
+  "html":  "text/html",
+  "shtml": "text/html",
+  "css":   "text/css",
+  "js":    "text/javascript",
+  "jpg":   "image/jpeg",
+  "png":   "image/png",
+  "gif":   "image/gif"
+};
+
+function getMIMEType(url) {
+  return FILETYPES[url.match(/^\/[a-zA-Z0-9\/]*.([a-z]*)$/)[1]];
+}
+
 http.createServer(function(request, response) {
   // If no file specified, return index.html
   if (request.url === "/") {
@@ -8,6 +22,7 @@ http.createServer(function(request, response) {
       Location: "/index.html"
     });
   }
-  
+
+  console.log(getMIMEType(request.url));
   response.end();
 }).listen(3000);
