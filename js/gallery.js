9b4edfc00103875bf9ad27be2025ffe56216c046
dftbaustin <austinddd63z@gmail.com>
2018-04-30 09:03:41 -0400

    Moving gallery back buttons to top of each ride

--- js/gallery.js
+++ js/gallery.js
@@ -3,6 +3,11 @@ var listOfRides = [
 "Techno_Torando", "Devilish_Derby", "Seagrams_escape", "Night_Train"
 ];
 
+window.onload = function(e) {
+   for (var i = 0; i < document.getElementsByClassName("back").length; i++)
+      document.getElementsByClassName("back")[i].onclick = goBack;
+}
+
 function mouseOver(rideNum) {
    for (var i = 0; i < listOfRides.length; i++) {
       if (i == rideNum){
@@ -18,25 +23,25 @@ function mouseOut() {
 }
 
 function displayRideInfo(rideNum){
-   if (rideNum == -1){// go back to dispaling all rides, this means the user is on onr ride
-      for (var i = 0; i < listOfRides.length; i++) {
-         document.getElementById(listOfRides[i]).style.display = "block";
-         document.getElementById(listOfRides[i]).style.height = "45%";
-         document.getElementById(listOfRides[i]).style.width = "20%";
-         // document.getElementById(listOfRides[i]+"_pic").style.height = "70%";
-         // document.getElementById(listOfRides[i]+"_pic").style.width = "100%";
-         document.getElementById(listOfRides[i]+"_desc").classList.remove("visible");
+   for (var i = 0; i < listOfRides.length; i++) {
+      if( i != rideNum){
+         document.getElementById(listOfRides[i]).style.display = "none";
       }
-   }else{
-      for (var i = 0; i < listOfRides.length; i++) {
-         if( i != rideNum){
-            document.getElementById(listOfRides[i]).style.display = "none";
-         }
-      }
-      document.getElementById(listOfRides[rideNum]).style.height = "95%";
-      document.getElementById(listOfRides[rideNum]).style.width = "95%";
-      // document.getElementById(listOfRides[rideNum]+"_pic").style.height = "25%";
-      // document.getElementById(listOfRides[rideNum]+"_pic").style.width = "25%";
+   }
+   document.getElementById(listOfRides[rideNum]).style.height = "95%";
+   document.getElementById(listOfRides[rideNum]).style.width = "95%";
+   // document.getElementById(listOfRides[rideNum]+"_pic").style.height = "25%";
+   // document.getElementById(listOfRides[rideNum]+"_pic").style.width = "25%";
    document.getElementById(listOfRides[rideNum]+"_desc").classList.add("visible");
 }
+
+function goBack(e) {
+   for (var i = 0; i < listOfRides.length; i++) {
+      document.getElementById(listOfRides[i]).style.display = "block";
+      document.getElementById(listOfRides[i]).style.height = "45%";
+      document.getElementById(listOfRides[i]).style.width = "20%";
+      // document.getElementById(listOfRides[i]+"_pic").style.height = "70%";
+      // document.getElementById(listOfRides[i]+"_pic").style.width = "100%";
+      document.getElementById(listOfRides[i]+"_desc").classList.remove("visible");
+   }
 }
